b7ab2ca349abd374dec0eaff888431a1d5544a25
Fabiola971 <fabiola.barul@gmail.com>
2024-01-10 23:18:24 +0100

    new version

--- js/app.js
+++ js/app.js
@@ -11,7 +11,50 @@ const testimonys = [
         name: 'John Tarkpor',
         job: 'Junior Front-end Developer',
         img: 'image-john.jpg',
-        tips: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
-      
+        tips: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',  
     }
 ];
+// const slide
+const tips = document.getElementsByClassName('tips');
+console.log(tips)
+const n = document.getElementsByClassName('name');
+const job = document.getElementsByClassName('job');
+const img = document.getElementsByClassName('view');
+
+
+// const button
+const icon = document.querySelectorAll('.icon');
+console.log(icon)
+const iconPrev = document.getElementsByClassName('icon-prev');
+
+const iconNext = document.getElementsByClassName('icon-next');
+
+
+
+/*
+let currentSlide = 0;
+const item = testimonys[currentSlide];
+    tips.textContent = item.tips;
+    n.textContent = item.name;
+    job.innerHtml = item.job;
+    img.src = item.img;
+window.addEventListener('DOMContentLoaded', () =>{
+    console.log('Hello good morning');
+    // preview();
+    const item = testimonys[currentSlide];
+    tips.textContent = item.tips;
+    n.textContent = item.name;
+    job.innerHtml = item.job;
+    img.src = item.img;
+});
+
+// first view
+
+function preview(){
+    const item = testimonys[currentSlide];
+    tips.textContent = item.tips;
+    n.textContent = item.name;
+    job.textContent = item.job;
+    img.src = item.img;
+}
+*/
