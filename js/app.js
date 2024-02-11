06f34e42e74cc49b471f623b82f6185ccd4a8594
Fabiola971 <vanillesdesiles971@yahoo.fr>
2024-02-12 00:33:32 +0100

    slide movement ?

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const testimonys = [
         id: 2,
         name: 'John Tarkpor',
         job: 'Junior Front-end Developer',
-        img: 'image-john.jpg',
+        img: 'images/image-john.jpg',
         text: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',  
     }
 ];
@@ -22,20 +22,19 @@ const n = document.querySelector('.name');
 const job = document.querySelector('.job');
 const img = document.querySelector('.view');
 
-const slides = document.querySelectorAll('.slide');
-console.log(icon)
-const iconPrev = document.querySelector('.icon-prev');
-const iconNext = document.querySelector('icon-next');
+const slidesContainer = [...document.querySelectorAll('#slideContainer')];
+const iconPrev = [...document.querySelectorAll('.icon-prev')];
+const iconNext = [...document.querySelectorAll('.icon-next')];
 
 // set starting slide
 let currentSlide = 0;
-
+/*
 iconPrev.addEventListener('click', () =>{
     console.log('ok');
 })
-
+*/
 // load initial slide
-window.addEventListener('DOMContentLoaded', () =>{
+window.addEventListener('DOMContentLoaded',  () =>{
     console.log('Hello good morning');
     const item = testimonys[currentSlide];
     tips.textContent = item.text;
@@ -53,3 +52,21 @@ function showPerson(person){
     job.textContent = item.job;
     img.src = item.img;
 }
+
+slidesContainer.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    console.log(containerDimensions)
+    let containerWidth = containerDimensions.width;
+    console.log(containerWidth)
+    
+    
+    iconPrev[i].addEventListener('click', () =>{
+        console.log('prev');
+        item.scrollLeft -= containerWidth;
+    })
+
+    iconNext[i].addEventListener('click', () =>{
+        console.log('next');
+        item.scrollLeft += containerWidth;
+    })
+})
